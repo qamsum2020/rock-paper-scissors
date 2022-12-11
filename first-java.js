12903094e8bc71186255826c6cac40dfa945650a
qamsum2020 <qamsum@gmail.com>
2022-12-11 12:17:27 +0500

    Final complete code

--- first-java.js
+++ first-java.js
@@ -1,1 +1,71 @@
-console.log("Hello, Qamar! Welcome to JavaScript");
+// Create a function to get computer choice.
+function getComputerChoice() {
+  
+  let computerSelection = Math.floor(Math.random() * 3);
+
+      if(computerSelection === 0) {
+        computerSelection = "Rock";
+      }
+      else if(computerSelection === 1) {
+        computerSelection = "Paper";
+      }
+      else if(computerSelection === 2) {
+        computerSelection = "Scissors";
+      }
+      return computerSelection;
+}
+
+function playRound(computer, player){
+
+  var computer = getComputerChoice();
+  var player = prompt("Please enter your choice");
+
+  if (computer === player) {
+
+    var result = "It's a Tie!";
+
+  } 
+  
+  else if ((computer === "Rock" && player === "Scissors") || 
+          (computer === "Scissors" && player === "Paper") || 
+          (computer === "Paper" && player === "Rock")) {
+    
+    result = "Computer Won!";
+  }
+  
+  else {
+    result = "You Won!";
+  }
+  
+    console.log(result);
+    return result;
+}
+
+function game(){
+
+  var roundWinner = '';
+  var computerTotal = 0;
+  var yourTotal = 0;
+
+  for (let i = 0; i < 5; i++){
+    
+    roundWinner = playRound();
+
+    if (roundWinner === 'Computer Won!') {
+      computerTotal ++;
+    } else if (roundWinner === 'You Won!') {
+      yourTotal ++;
+    }
+    
+    var finalWinner = '';
+    
+    if (computerTotal > yourTotal) {
+      finalWinner = 'The Computer is Winner!';
+    } else if (computerTotal < yourTotal) {
+      finalWinner = 'Congrats! You are the Winner!';
+    } else if (computerTotal === yourTotal) {
+      finalWinner = 'Its a Tie, Try again!';
+    }
+  }
+  return finalWinner;
+}
